57ec4d1edab7dc1e34d75b84184cb9f91f98f46e
NighttimeFox <vic.verstraete2@gmail.com>
2014-12-09 15:57:33 +0100

    project creation final
    Eindelijk.
    De naam van de "main" solution is ook veranderd naar "Hall"

--- source/Hall/Program.cs
+++ source/Hall/Program.cs
@@ -15,22 +15,38 @@ namespace main
     {
         static void OnClose(object sender, EventArgs e) 
         {
-            RenderWindow window = (RenderWindow)sender;
-            window.Close();
+            RenderWindow win = (RenderWindow)sender;
+            win.Close();
+        }
+
+        static void OnResize(object sender, EventArgs e)
+        {
+            
         }
  
         static void Main() 
         {
-            RenderWindow app = new RenderWindow(new VideoMode(800, 600), "SFML Works!");
-            app.Closed += new EventHandler(OnClose);
+            
+            RenderWindow window = new RenderWindow(new VideoMode(800, 600), "SFML Works!");
+            window.SetFramerateLimit(50);
+            window.Closed += new EventHandler(OnClose);
+            window.Resized += new EventHandler<SizeEventArgs>(OnResize);
             Color windowColor = new Color(0, 192, 255);
-          
-            while (app.IsOpen()) 
+
+            Console.WriteLine("entering main loop");
+            while (window.IsOpen()) 
             {
-               app.DispatchEvents();
-               app.Clear(windowColor);
-               app.Display();
+               window.DispatchEvents();
+               window.Clear(windowColor);
+               window.Display();
             }
+        }
+
+        static void init()
+        {
+            Console.WriteLine("begin init");
+
+            Console.WriteLine("init succesfull");
         }
     }
 }
